c64be99cdf478cc83015ed912ee8edd97e93f21c
Runoi <75607022+Runoi@users.noreply.github.com>
2025-05-02 12:42:40 +0300

    .

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pyrogram import Client
 from handlers.commands import register_handlers
 from utils.config import Config
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # Настройка логирования
 logging.basicConfig(
